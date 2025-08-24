5ddc8218f80d2b0e5323faf8692fe4f59a472e3c
Brian <brobertjernigan@gmail.com>
2025-08-23 22:19:43 -0400

    Combo
    Need to fix bug with jumping

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -5,6 +5,11 @@ public class PlayerCombat : MonoBehaviour
     [Header("References")] 
     [SerializeField] private Animator _animator;
 
+    [Header("Colliders")] 
+    [SerializeField] private CapsuleCollider2D _punchOneCollider;
+    [SerializeField] private CapsuleCollider2D _punchTwoCollider;
+    [SerializeField] private CapsuleCollider2D _punchThreeCollider;
+    
     [Header("Combo Settings")] 
     [SerializeField] private float _comboResetTime = 1f;
 
@@ -30,5 +35,55 @@ public class PlayerCombat : MonoBehaviour
     {
         _attackIndex = 0;
         _animator.SetInteger(AttackIndexHash, 0);
+        
+        DisableAllColliders();
+    }
+
+    public void StartPunchOneFrames()
+    {
+        ToggleCollider(_punchOneCollider, true);
+        ToggleCollider(_punchTwoCollider, false);
+        ToggleCollider(_punchThreeCollider, false);
+    }
+
+    public void EndPunchOneFrames()
+    {
+        ToggleCollider(_punchOneCollider, false);
+    }
+
+    public void StartPunchTwoFrames()
+    {
+        ToggleCollider(_punchTwoCollider, true);
+        ToggleCollider(_punchOneCollider, false);
+        ToggleCollider(_punchThreeCollider, false);
+    }
+
+    public void EndPunchTwoFrames()
+    {
+        ToggleCollider(_punchTwoCollider, false);
+    }
+
+    public void StartPunchThreeFrames()
+    {
+        ToggleCollider(_punchThreeCollider, true);
+        ToggleCollider(_punchOneCollider, false);
+        ToggleCollider(_punchTwoCollider, false);
+    }
+
+    public void EndPunchThreeFrames()
+    {
+        ToggleCollider(_punchThreeCollider, false);
+    }
+
+    private void ToggleCollider(CapsuleCollider2D capsule, bool state)
+    {
+        capsule.enabled = state;
+    }
+
+    private void DisableAllColliders()
+    {
+        _punchOneCollider.enabled = false;
+        _punchTwoCollider.enabled = false;
+        _punchThreeCollider.enabled = false;
     }
 }
